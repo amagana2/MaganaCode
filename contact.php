2c3fa6e6db833eac4c2a63b6e43e58f0e4fe187f
Andrew <andrew.magana@adp.com>
2017-10-23 13:48:13 -0400

    Updated skills page, added favicon.ico, and modified the PHP file after testing. Site is complete as of 10/23/2017 -- this is version 2.

--- contact.php
+++ contact.php
@@ -1,24 +1,16 @@
 <?php
 /*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-
-require 'PHPMailer-master/PHPMailerAutoload.php';
-
-/*
-*  CONFIGURE EVERYTHING HERE
-*/
+ *  CONFIGURE EVERYTHING HERE
+ */
 
 // an email address that will be in the From field of the email.
-$fromEmail = 'demo@domain.com';
-$fromName = 'Contact Form Sender';
+$from = 'maganacode@contact_form.com';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'amagana286@gmail.com';
-$sendToName = 'Andrew';
+$sendTo = 'amagana286@gmail.com';
 
 // subject of the email
-$subject = 'New message from your site!';
+$subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
@@ -31,8 +23,8 @@ $okMessage = 'Contact form successfully submitted. Thank you, I will get back to
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
 /*
-*  LET'S DO THE SENDING
-*/
+ *  LET'S DO THE SENDING
+ */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
@@ -42,40 +34,30 @@ try
 
     if(count($_POST) == 0) throw new \Exception('Form is empty');
 
-    $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
-    $emailTextHtml .= "<table>";
+    $emailText = "You have a new message from your site! \n=============================\n";
 
     foreach ($_POST as $key => $value) {
         // If the field exists in the $fields array, include it in the email
         if (isset($fields[$key])) {
-            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+            $emailText .= "$fields[$key]: $value\n";
         }
     }
-    $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Ondrej</p>";
 
-    $mail = new PHPMailer;
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
 
-    $mail->setFrom($fromEmail, $fromName);
-    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
-    $mail->addReplyTo($from);
-
-    $mail->isHTML(true);
-
-    $mail->Subject = $subject;
-    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-
-
-    if(!$mail->send()) {
-        throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
-    }
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
 catch (\Exception $e)
 {
-    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
 
